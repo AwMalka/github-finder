d8c41e8474cf3a848cf86083fc14881e41a0338f
Avi Malka <avima@payoneer.com>
2021-05-19 08:52:24 +0300

    about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,8 +5,13 @@ const About = () => {
     <Fragment>
       <div>
         <h1>About This App</h1>
-        <p>App to searcg Github users</p>
+        <p>App to search Github users</p>
         <p>Version: 1.0.0</p>
+        <p>
+          Github repo:{' '}
+          <a href='https://github.com/AwMalka/github-finder'>github-finder</a>
+        </p>
+        <p>Author: Avi Malka</p>
       </div>
     </Fragment>
   );
